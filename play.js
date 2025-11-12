bc458717b91021d4af84c2aec727889da6af3b47
ahmed faris <ahmedfarisuw05@gmail.com>
2025-11-12 16:03:09 +0100

    first function on js

--- play.js
+++ play.js
@@ -3,12 +3,55 @@
 let numberCards = document.getElementById('numbercards');
 let collectionArr = JSON.parse(localStorage.getItem("collectionArr")) || [];
 let cartes_container=document.getElementById('cartes-container')
-
+let choose_btn=document.getElementById("choosebtn")
+let main_container=document.getElementById("main-container")
 numberCards.innerHTML = `<p>Deck (${collectionArr.length})</p>`;
 
+function randomcards(Arr, count=0){
+    let collcopy=[...Arr].sort(()=>0.5 - Math.random());
+    return collcopy.slice(0,count);
+}
+
+function fillContainers(){
+    const main_container=document.getElementById("main-container");
+    main_container.innerHTML="";
+    let randomCard=randomcards(collectionArr,5);
+    console<
+    randomCard.forEach((card)=>{
+        const div=document.createElement("div");
+        div.className = "card-item w-full h-auto bg-card-bg bg-center bg-no-repeat flex flex-col justify-start relative";
+        div.innerHTML=` 
+            <div class="bg-card-header bg-center bg-contain bg-no-repeat w-auto m-auto absolute top-0 left-4 flex justify-center items-center text-black">
+                
+                <p>${c.name}</p>
+            </div>
+            <img class="mt-10 h-[15vh] w-auto mx-auto" src="${
+              c.img
+            }" alt="card-img">
+            
+               
+    `;
+    main_container.append(div);
+    })
+
+}
+choose_btn.addEventListener("click", ()=>{
+  if(collectionArr.length<5){
+    console.warn("pas assez de cartes dans la collection")
+     }
+  else{
+        fillContainers();
+    }
+        
+   
+ 
+  
+
+
+} );
 function fetchCard(){
    collectionArr.forEach((c)=>{
-    let div= document.createElement("div");
+    let div=document.createElement("div");
 
     div.className=`carte-${c.id}`
     const html=` <div class="w-full h-auto
